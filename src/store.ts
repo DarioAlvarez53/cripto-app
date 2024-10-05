5a2077c4120313b08bf0d6b7ef4128871f8459b2
Dario Alvarez <daralv53@gmail.com>
2024-10-05 11:08:39 -0600

    Moviendo la peticion a un archivo independiente

--- src/store.ts
+++ src/store.ts
@@ -1,24 +1,11 @@
 import { create } from "zustand"
 import { devtools } from "zustand/middleware"
-import axios from "axios";
-import { CryptoCurrenciesRespnseSchema } from "./schemas/crypto-schema";
 import { CryptoCurrency } from "./types";
+import { getCryptos } from "./services/CryptoService";
 
 type CryptoStore = {
     criptoCurrencies: CryptoCurrency[]
     fetchCryptos:() => Promise<void>
-}
-
-async function getCryptos() {
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
-    const {data: {Data}} = await axios(url)
-    // console.log(Data);
-    const result = CryptoCurrenciesRespnseSchema.safeParse(Data)
-    // console.log(result);
-    if(result.success) {
-        return result.data
-    }
-    
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
